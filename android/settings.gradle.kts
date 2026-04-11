ccad89f685398af631d8b66e3b1d0a41572f6ac5
Calvin Rumba <calvin@wapipay.com>
2026-04-11 16:12:10 +0300

    updates

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -17,16 +17,9 @@ pluginManagement {
 }
 
 plugins {
-    id("dev.flutter.flutter-gradle-plugin") version "1.0.0" apply false
-    id("com.android.application") version "8.7.0" apply false
+    id("dev.flutter.flutter-plugin-loader") version "1.0.0"
+    id("com.android.application") version "8.9.1" apply false
     id("org.jetbrains.kotlin.android") version "2.1.0" apply false
 }
 
 include(":app")
-
-val flutterSdkPath = run {
-    val properties = java.util.Properties()
-    file("local.properties").inputStream().use { properties.load(it) }
-    properties.getProperty("flutter.sdk") ?: error("flutter.sdk not set in local.properties")
-}
-apply(from = file("$flutterSdkPath/packages/flutter_tools/gradle/app_plugin_loader.gradle"))
